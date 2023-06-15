71845fa2fee17f6f09ba534f9acfaeef99e56b77
nahid08 <drmc.nahid@gmail.com>
2023-06-16 03:00:36 +0600

    add role file while starting the application

---
+++ security/src/main/java/com/example/security/component/RoleEntry.java
@@ -0,0 +1,35 @@
+package com.example.security.component;
+
+import com.example.security.model.ERole;
+import com.example.security.model.Role;
+import com.example.security.repository.RoleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@Component
+public class RoleEntry implements CommandLineRunner {
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Testing Component annotation");
+
+        List<ERole> roles = new ArrayList<>(Arrays.asList(ERole.ROLE_ADMIN, ERole.ROLE_MODERATOR, ERole.ROLE_USER));
+
+        roles.forEach(role -> {
+            Optional<Role> x = roleRepository.findByName(role);
+            if(x.isEmpty()) {
+                roleRepository.save(new Role(role));
+            }
+        });
+
+    }
+}
